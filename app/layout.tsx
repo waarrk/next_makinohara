43ea77d7e3d48ad242bde9a2a63747aee3aa92a6
Washio Yusaku <washioyusaku@icloud.com>
2023-01-14 14:53:55 +0900

    design: フォント設定 (#12)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,30 @@
 import '@/styles/globals.css'
 
-export default function RootLayout({
-  children,
-}: {
+import { Inter as FontSans } from '@next/font/google'
+
+import { ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+interface RootLayoutProps {
   children: React.ReactNode
-}) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang='en'>
+    <html
+      lang='ja'
+      className={twMerge(
+        clsx(
+          'bg-white font-sans text-slate-900 antialiased',
+          fontSans.variable,
+        ),
+      )}
+    >
       <head />
       <body>{children}</body>
     </html>
