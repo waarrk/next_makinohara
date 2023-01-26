f92125d25beef6eac15e6320ecddc85ce74cd5f6
Washio Yusaku <washioyusaku@icloud.com>
2023-01-26 16:26:06 +0900

    add: ヘッダStorybook (#18)

---
+++ components/header.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Header from './header'
+
+export default {
+  title: 'Example/Header',
+  component: Header,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Header>
+
+const Template: ComponentStory<typeof Header> = (args) => <Header />
+
+export const Normal = Template.bind({})
