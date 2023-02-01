87dcc30fcd7cecdb607eff41e0a2fd77725d2393
Washio Yusaku <washioyusaku@icloud.com>
2023-02-02 00:12:41 +0900

    remove: favicon (#10)

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,6 @@ export default function Head() {
       <title>waarrk&apos;s playground</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <meta name='description' content='waarrkの遊び場です' />
-      <link rel='icon' href='/favicon.ico' />
     </>
   )
 }
