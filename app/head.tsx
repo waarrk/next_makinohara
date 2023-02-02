3d03d3a595de113cbdcda23eb3402700ddc5b890
Washio Yusaku <washioyusaku@icloud.com>
2023-02-03 01:22:57 +0900

    fix: 大文字小文字問題 (#32)

--- app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>waarrk&apos;s playground</title>
-      <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta name='description' content='waarrkの遊び場です' />
-    </>
-  )
-}
